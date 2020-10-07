be95d607393dc6aa0e82ac3a12f8a4de652e0cd3
DerrickDarku <derrickalberto@ymail.com>
2020-10-07 13:47:42 +0000

    class based to functional

---
+++ App.js
@@ -0,0 +1,44 @@
+// import { StatusBar } from 'expo-status-bar';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TextInput,} from 'react-native';
+
+
+class App extends Component{
+  render(){
+    return (
+      <View style={styles.container}>
+          <TextInput
+           style={styles.input} 
+           placeholder="email" 
+           autoCapitalize="none"
+           autoCorrect={false}
+          //  secureTextEntry={true}
+          value="good"
+          onChangeText={(text)=>{
+            console.log(text)
+          }}
+           />
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+   marginTop:100,
+   backgroundColor:"red"
+  },
+  input:{
+    backgroundColor:"yellow",
+    height: 60,
+    marginLeft:15
+  }
+
+    
+ 
+});
+
+
+export default App
